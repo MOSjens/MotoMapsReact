b67ccbc40d63a5ed199b568c35ab99bfbbf5d0af
Jens Reinhart <ucjhu@student.kit.edu>
2020-10-15 08:59:51 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MotoMaps on React</h1>
+      <h2>Author: Jens Reinhart</h2>
+    </div>
+  );
+}
+
+export default App;
