9d356562bdcfde58a1f993ade521b42d07bc74ba
Jens Reinhart <ucjhu@student.kit.edu>
2020-10-15 08:34:06 +0200

    Updates

---
+++ precache-manifest.2209afcd921733202fd273925e4c3927.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdfe4038b7181a4ca7930b1b9cc4371e",
+    "url": "/MotoMapsReact/index.html"
+  },
+  {
+    "revision": "6b427343dd174d041a3c",
+    "url": "/MotoMapsReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f48d79ba12b3cf1f80c1",
+    "url": "/MotoMapsReact/static/js/2.ecdfc21d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/MotoMapsReact/static/js/2.ecdfc21d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b427343dd174d041a3c",
+    "url": "/MotoMapsReact/static/js/main.0c31c8fd.chunk.js"
+  },
+  {
+    "revision": "054362625a395b994b83",
+    "url": "/MotoMapsReact/static/js/runtime-main.9a0364a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MotoMapsReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
